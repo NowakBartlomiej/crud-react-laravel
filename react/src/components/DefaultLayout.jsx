d337dec7875aab7765a6f9a4aad8075f59b0dc7d
Bartlomiej <x@x>
2023-02-10 18:38:53 +0100

    Implemendet login, signup and logout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, Navigate, Outlet } from 'react-router-dom'
+import axiosClient from '../../axios-client'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const DefaultLayout = () => {
-  const {user, token} = useStateContext()
+  const {user, token, setUser, setToken} = useStateContext()
 
   if (!token) {
 
@@ -12,7 +13,20 @@ const DefaultLayout = () => {
   
   const onLogout = (ev) => {
     ev.preventDefault()
+
+    axiosClient.post('/logout')
+      .then(() => {
+        setUser({})
+        setToken(null);
+      })
   }
+
+  useEffect(() => {
+    axiosClient.get('/user')
+      .then(({data}) => {
+        setUser(data);
+      })
+  }, [])
 
   return (
     <div id='defaultLayout'>
